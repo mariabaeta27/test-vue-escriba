57f25d6b1e815c49134fb9a626737c371a9f57ed
Maria Baeta <m.martinsdg@gmail.com>
2023-10-24 16:07:01 -0300

    feat: Created Routes

---
+++ src/views/OrdersView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="orders">
+    <h1>This is an orders page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .orders {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/UsersView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="users">
+    <h1>This is an users page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .users {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
